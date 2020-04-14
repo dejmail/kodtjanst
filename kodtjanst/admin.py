db095ecbddfca97df419ee28b8c10d54fb8b0fe0
Liam Jed Thompson <dejmail@gmail.com>
2020-04-14 13:41:55 +0200

    added the changes to the model plurals and dropdown choices

--- kodtjanst/admin.py
+++ kodtjanst/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Kodverk, Kodtext, ExternaKodverk, MappadTillKodtext
 
+admin.site.site_header = "OLLI Kodtjänst Admin"
+admin.site.site_title = "OLLI Kodtjänst Admin Portal"
+admin.site.index_title = "Välkommen till OLLI Kodtjänst Portalen"
+
 class KodtextManager(admin.ModelAdmin):
 
     list_display = ('kodtext', 
