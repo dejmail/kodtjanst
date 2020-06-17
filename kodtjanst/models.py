ecfc240e5f3323c0554e99d3fc367e4d5e815ea4
Liam Jed Thompson <dejmail@gmail.com>
2020-06-17 13:58:46 +0200

    added the django mysql app to add support for a json field that is not supported in the normal Django codebase and I deleted the previous migrations and started from zero again

--- kodtjanst/models.py
+++ kodtjanst/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
-from .fields import JSONField
+#from .fields import JSONField
+from django_mysql.models import JSONField, Model
+
 
 class Kodtext(models.Model):
 
--- settings/production.py
+++ settings/production.py
@@ -10,10 +10,18 @@ DATABASES = {
         'PASSWORD': 'YqvyYGm5cJMLmzt',
         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
-        'DEFAULT-CHARACTER-SET' : 'utf8',
+        #'DEFAULT-CHARACTER-SET' : 'utf8',
+        'OPTIONS': {
+            # Tell MySQLdb to connect with 'utf8mb4' character set
+            'charset': 'utf8mb4',
+        },
+        # Tell Django to build the test database with the 'utf8mb4' character set
+        'TEST': {
+            'CHARSET': 'utf8mb4',
+            'COLLATION': 'utf8mb4_unicode_ci',
+        }
     }
 }
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
