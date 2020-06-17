cfcd77ce9c315b7de2ae63748ffc87c15d671434
Liam Jed Thompson <dejmail@gmail.com>
2020-06-17 10:04:09 +0200

    removed all previous migrations as we dont need them and pushed all the most recent changes so that the app is back to initial

--- kodtjanst/admin.py
+++ kodtjanst/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
 from .models import Kodverk, Kodtext
 
-from .models import Kodverk, Kodtext, ExternaKodverk, MappadTillKodtext
+from .models import Kodverk, Kodtext, ExternaKodverk, MappadTillKodtext, Nyckelord
 from import_export import resources
 
 admin.site.site_header = "OLLI Kodtjänst Admin"
@@ -21,10 +21,10 @@ class KodverkResource(resources.ModelResource):
     class Meta:
         model = Kodverk
 
-class KodverkManager(ImportExportModelAdmin):
+class KodverkManager(ImportExportModelAdmin):  
 
     list_display = ('kodverk_variant',
-                    'urval_reference',
+                    'urval_referens',
                     'syfte',
                     'rubrik_på_kodverk',
                     'kort_beskrivning',
@@ -34,11 +34,11 @@ class KodverkManager(ImportExportModelAdmin):
                     'identifier',
                     'version',
                     'källa',
-                    'typ_av_kodverk',
-                    'ämnesområde',
+                    'kodverk_variant',
+                    'ämne',
                     'instruktion_för_kodverket',
                     'ändrad_av',
-                    'ägare',
+                    'ägare_av_kodverk',
                     'ansvarig',
                     'mappning_för_rapportering',
                     'språk',
@@ -52,4 +52,5 @@ admin.site.register(Kodverk, KodverkManager)
 admin.site.register(Kodtext,KodtextManager)
 admin.site.register(ExternaKodverk)
 admin.site.register(MappadTillKodtext)
+admin.site.register(Nyckelord)
 
--- kodtjanst/models.py
+++ kodtjanst/models.py
@@ -1,21 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
-
+from .fields import JSONField
 
 class Kodtext(models.Model):
 
     class Meta:
         verbose_name_plural = "Kodtexter"
     
-    kodtext = models.CharField(max_length=255)
-    kod = models.CharField(max_length=255)
-    definition = models.TextField(max_length=500, null=True, blank=True)
     andra_definition = models.TextField(max_length=500, null=True, blank=True)
+    definition = models.TextField(max_length=500, null=True, blank=True)
+    extra_data = JSONField(null=True)
+    kod = models.CharField(max_length=255, null=True)
+    kodtext = models.CharField(max_length=255)
     kodverk = models.ForeignKey(to='Kodverk', to_field='id', on_delete=models.PROTECT, default=11)
-    position = models.PositiveIntegerField()
-    ändrat_av = models.ForeignKey(User, on_delete=models.PROTECT, blank=True, null=True)
+    kommentar = models.TextField(null=True)
+    position = models.PositiveIntegerField(null=True)
     status = models.CharField(max_length=50, blank=True, null=True)
+    ändrad_av = models.ForeignKey(User, on_delete=models.PROTECT, blank=True, null=True)
 
     def __str__(self):
         return self.kodtext
@@ -25,9 +27,11 @@ class MappadTillKodtext(models.Model):
     class Meta:
         verbose_name_plural = "Mappad Kodtexter"
 
-    code = models.ForeignKey(to='Kodtext', to_field='id', on_delete=models.PROTECT)
-    mappad_id = models.CharField(max_length=255)
-    mappad_kodverk = models.URLField()
+    kodtext = models.ForeignKey(to='Kodtext', to_field='id', on_delete=models.PROTECT)
+    mappad_id = models.CharField(max_length=255, null=True)
+    mappad_text = models.CharField(max_length=255, null=True)
+    resolving_url = models.URLField()
+    kommentar = models.TextField(null=True)
 
 class ExternaKodverk(models.Model):
 
@@ -66,33 +70,57 @@ class Kodverk(models.Model):
                    ('Alfa respons','Alfa respons'), 
                    ('Urval','Urval')]
 
-    kodverk_variant = models.CharField(null=False, max_length=12, choices=kodverk_typ)
-    urval_reference = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
-    syfte = models.TextField(max_length=1000, null=True)
-    rubrik_på_kodverk = models.CharField(max_length=255, null=True)
-    kort_beskrivning = models.TextField(max_length=1000, null=True)
+    ansvarig =  models.ForeignKey(User, on_delete=models.PROTECT, related_name='ansvarig_person', null=True, blank=True)
+    beskrivning_av_informationsbehov = models.TextField(null=True)
     giltig_från = models.DateField(null=True)
     giltig_tom = models.DateField(null=True)
-    kodschema = models.CharField(max_length=255,null=True)
+    kommentar = models.TextField(null=True)
     identifier = models.CharField(max_length=255,null=True)
-    version = models.FloatField(validators=[MinValueValidator(0.01)], null=True)
-    källa = models.CharField(max_length=255,null=True)
-    typ_av_kodverk = models.CharField(max_length=255,null=True, choices=kodverk_typer)
-    ämnesområde = models.CharField(max_length=255,null=True)
-    #Länk till Kodverk - det blir url
     instruktion_för_kodverket = models.CharField(max_length=255,null=True)
-    ändrad_av = models.ForeignKey(User, on_delete=models.PROTECT)
-    ägare = models.CharField(max_length=255,null=True, choices=kodverk_ägare)
-    ansvarig = models.CharField(max_length=255,null=True)
-    mappning_för_rapportering = models.BooleanField(null=True)
-    #Sökord
-    språk = models.CharField(max_length=25, choices=SPRÅK_CHOICES, default='svenska',null=True)
-    uppdateringsintervall =  models.CharField(max_length=20, null=True, choices=intervall)
-    version_av_källa = models.CharField(max_length=50,null=True)
-    system_som_använderkodverket = models.CharField(max_length=255,null=True)
+    extra_data = JSONField(null=True)
     kategori = models.CharField(max_length=255,null=True)
-    #antal koder i kodverket
-    beskrivning_av_informationsbehov = models.TextField(null=True)
+    kodschema = models.CharField(max_length=255,null=True)
+    kodverk_variant = models.CharField(max_length=12, choices=kodverk_typ, null=True, blank=True)
+    kort_beskrivning = models.TextField(max_length=1000, null=True)
+    källa = models.CharField(max_length=255,null=True)
+    mappning_för_rapportering = models.BooleanField(null=True)
+    nyckelord = models.CharField(max_length=255, null=True, blank=True)
+    rubrik_på_kodverk = models.CharField(max_length=255, null=True)
+    senaste_ändring = models.DateField(blank=True, null=True)
+    språk = models.CharField(max_length=25, choices=SPRÅK_CHOICES, default='svenska',null=True)
+    status = models.CharField(max_length=25, blank=True ,null=True)
+    syfte = models.TextField(max_length=1000, null=True)
+    system_som_använderkodverket = models.CharField(max_length=255,null=True)
+    uppdateringsintervall =  models.CharField(max_length=20, null=True, choices=intervall)
+    urval_referens = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+    version = models.FloatField(validators=[MinValueValidator(0.01)], null=True)
+    version_av_källa = models.CharField(max_length=50,null=True)
+    ägare_av_kodverk = models.CharField(max_length=255,null=True, choices=kodverk_ägare)
+    #ämnesområde = models.CharField(max_length=255,null=True)
+    ändrad_av = models.ForeignKey(User, on_delete=models.PROTECT, related_name='ändrad_av_person')
 
     def __str__(self):
         return self.rubrik_på_kodverk
+
+class Nyckelord(models.Model):
+    
+    class Meta:
+        verbose_name_plural = "Nyckelord"
+
+    kodverk_id = models.ForeignKey(to='Kodtext', to_field='id', on_delete=models.PROTECT)
+    nyckelord = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.nyckelord
+
+class Ämne(models.Model):
+    class Meta:     
+        verbose_name_plural = "Ämnesområde"
+
+    kodverk = models.ForeignKey("Kodverk", to_field="id", on_delete=models.PROTECT, blank=True, null=True)
+    domän_id = models.AutoField(primary_key=True)
+    domän_kontext = models.TextField(null=True)
+    domän_namn = models.CharField(max_length=255)       
+
+    def __str__(self):
+        return self.domän_namn
