1828aa8bb26a8d08dab065f88fa885ec4bcf7593
Liam Jed Thompson <dejmail@gmail.com>
2020-06-26 10:55:40 +0200

    first working alpha version of KOLLI with all the associated modifications

--- kodtjanst/views.py
+++ kodtjanst/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib import admin
-from django.urls import path
+from django.urls import path 
+from django.db import connection, transaction
+
 from .forms import UserLoginForm
+from .models import Kodverk, Kodtext, MappadTillKodtext
 
 from pdb import set_trace
 
@@ -42,3 +45,65 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('/')
+
+def retur_general_sök(url_parameter):
+    cursor = connection.cursor()
+    ''' need to reduce the number of fields being returned, we are not using all of them,
+    but this also affects the parsing as it is position based, so need to be careful'''
+    sql_statement = f'''SELECT kodtjanst_kodverk.rubrik_på_kodverk,\
+                                nyckelord,\
+                                status,\
+                                syfte,\
+                                kodtjanst_ämne.domän_namn AS dömän_namn,\
+                                kodtjanst_kodtext.kodtext,\
+                                kodtjanst_kodtext.annan_kodtext,\
+                                kodtjanst_kodtext.definition,\
+                        FROM kodtjanst_kodverk\
+                            LEFT JOIN kodtjanst_kodtext\
+                                ON kodtjanst_kodtext.id = kodtjanst_koverkid\
+                            LEFT JOIN kodtjanst_ämne\
+                                ON kodtjanst_kodverk.id = kodtjanst_ämne_id\
+                        WHERE (kodtjanst_kodverk.rubrik_på_kodverk LIKE "%{url_parameter}%")\
+                        OR (kodtjanst_kodverk.syfte LIKE "%{url_parameter}%")\
+                        OR (kodtjanst_kodverk.nyckelord LIKE "%{url_parameter}%"\
+                        OR (kodtjanst_kodtext.kodtext LIKE "%{url_parameter}%"\
+                        OR (kodtjanst_kodtext.annan_kodtext LIKE "%{url_parameter}%"\
+                        OR (kodtjanst_kodtext.definition LIKE "%{url_parameter}%";'''
+    
+    # column_names = ['begrepp_id',
+    #                 'definition',
+    #                 'term',
+    #                 'begrepp_status', 
+    #                 'synonym_begrepp_id',
+    #                 'synonym',
+    #                 'synonym_status']
+
+    clean_statement = re.sub(re_pattern, ' ', sql_statement)
+    cursor.execute(clean_statement)
+    result = cursor.fetchall()
+    
+    return result
+
+def kodverk_view(request):
+
+    url_parameter = request.GET.get("q")
+    
+    if request.is_ajax():
+        #data_dict, return_list_dict = hämta_data_till_begrepp_view(url_parameter)
+        #mäta_sök_träff(sök_term=url_parameter,sök_data=return_list_dict, request=request)
+        data_dict = retur_general_sök(url_parameter)
+        return JsonResponse(data=data_dict, safe=False)
+
+    # elif request.method == 'GET':
+    #     data_dict, return_list_dict = hämta_data_till_begrepp_view(url_parameter)
+    #     return render(request, "term-results-partial.html", context=data_dict)
+
+    # elif request.method=='GET':
+    #     return render(request, "term_forklaring.html", context=template_context)
+    
+    else:
+        kodverk = Kodverk.objects.none()
+    return render(request, "kodverk.html", {})
+
+def kodverk_metadata_view(request):
+    return render(request, "kodverk_metadata_view.html", context)
---
+++ static/js/main.js
@@ -0,0 +1,137 @@
+const user_input = $("#user-input");
+const search_icon = $('#search-icon');
+const target_div = $('#mitten-span-middle-column');
+
+function endpoint_check() {
+
+    if (document.domain == "127.0.0.1") { 
+		const endpoint = 'kodtjanst';
+		return endpoint
+    } else {
+		const endpoint = document.URL;
+		return endpoint
+	}
+
+}
+
+const endpoint = endpoint_check();
+
+const delay_by_in_ms = 750
+let scheduled_function = true
+
+user_input.keyup(function () {
+	$("#mitten-span-middle-column").empty();
+
+	const request_parameters = {
+		
+		q: $(this).val() // value of user_input: the HTML element with ID user-input
+	}
+	
+	if (request_parameters.q.length > 1) {
+
+	var ajax_call = function (endpoint, request_parameters) {
+		$("#term_förklaring_tabell").remove();
+		$("#mitten-span-middle-column").empty();
+		var skapad_url = (endpoint + '?' + Object.keys(request_parameters) + '=' + Object.values(request_parameters));
+		console.log('skapad_url is', skapad_url)
+		$.getJSON(endpoint, request_parameters)
+			.done(response => {
+				console.log("document.URL", document.URL)
+		        console.log("endpoint", endpoint);
+				changeBrowserURL(response, skapad_url);
+				// fade out the target_div, then:
+				target_div.fadeTo('fast', 0).promise().then(() => {
+					// replace the HTML contents
+					target_div.html(response);
+					// fade-in the div with new contents
+					target_div.fadeTo('fast', 1);
+					// stop animating search icon
+					search_icon.removeClass('blink');
+					popStateHandler();
+				})
+			});
+		popStateHandler();
+	}
+} 
+	
+	// start animating the search icon with the CSS class
+	search_icon.addClass('blink')
+
+	// if scheduled_function is NOT false, cancel the execution of the function
+	if (scheduled_function) {
+		clearTimeout(scheduled_function)
+	}
+
+	// setTimeout returns the ID of the function to be executed
+	scheduled_function = setTimeout(ajax_call, delay_by_in_ms, endpoint, request_parameters)
+
+});
+
+document.body.addEventListener("click", function(e) {
+	// e.target was the clicked element
+	if(e.target && e.target.nodeName == "A") {
+    // Stop the browser redirecting to  the HREF value.
+    event.preventDefault();    
+    console.log("sending", e.target.id, "ID to URL", e.target.href);
+    // Attach event listeners for browser history and hash changes.
+    
+    //changeBrowserURL(null, e.target.href);            
+	// Get page and replace current content.
+	//debugger;
+	getPage(e.target.href);
+	popStateHandler();
+	}
+});
+
+function getPage(link_url) {
+
+	console.log('entering ajax getPage function');
+	$.ajax({
+		type: "GET",
+		dataType: "html",
+		url: link_url,
+	}).done(function(data, textStatus, jqXHR) {
+		//debugger;
+		$("#mitten-span-middle-column").empty();
+		var data = data.replace('\n','').replace('  ', '').replace('\"', "");
+		$('#mitten-span-middle-column').html(data.slice(1,-1));
+		changeBrowserURL(data, this.url);
+	}).fail(function(data,textStatus,jqXHR) {        
+		  $('#mitten-span-middle-column').html("Fel - Hoppsan! Jag får ingen definition från servern...finns ett problem..prova trycka Ctrl-Shift-R");
+		});
+	  };
+
+/*
+* Function to modify URL within browser address bar.
+*/
+
+function changeBrowserURL(data, href) {
+	// Change URL with browser address bar using the HTML5 History API.
+	if (history.pushState) {
+	  console.log('in changeBrowserURL function, changing URL to', href)
+	  // Parameters: data, page title, URL
+	  history.pushState(data, null, href);
+	}
+	
+   };
+
+/*
+ * Function to detect when back and forward buttons clicked.
+ *
+ * This function will allow us to load content on the fly, as
+ * the browser cannot re-render the AJAX content between state changes.
+ */
+function popStateHandler() {
+	// FF, Chrome, Safari, IE9.
+	if (history.pushState) {
+	  // Event listener to capture when user pressing the back and forward buttons within the browser.
+	  console.log('popStateHandler  - history.pushState variable exists');
+	  window.addEventListener("popstate", function(e) {
+		// Get the URL from the address bar and fetch the page.
+		console.log('popstateHandler eventlistener fired, next stop getPage function')
+		//debugger;
+		$('#mitten-span-middle-column').html(history.state);
+		//getPage(document.URL);
+	  });
+}
+};
