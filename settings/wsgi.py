7f7acdf12d73e2144f6c4bb8a90d710ff90e8a42
Liam Jed Thompson <dejmail@gmail.com>
2022-01-24 11:52:33 +0100

    needed a dev filter

--- settings/wsgi.py
+++ settings/wsgi.py
@@ -10,11 +10,17 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 import os
 import sys
 import socket
+import logging
+from kodtjanst.logging import setup_logging
+
+logger = logging.getLogger(__name__)
 
 from django.core.wsgi import get_wsgi_application
 
 if socket.gethostname() == 'suijin.oderland.com':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.production')
+elif 'dev' in os.getcwd():
+    logger.info('Using dev settings')
 else:
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.local')
 
