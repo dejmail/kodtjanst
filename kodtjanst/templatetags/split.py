92012c3f2200a03f1e7f78580fc223f9d891404e
Liam Jed Thompson <dejmail@gmail.com>
2022-06-23 11:17:22 +0200

    cleaned up the logic for viewing the Källa as this often pointed towards the underlag dokument instead of a link specified in Källa

---
+++ kodtjanst/templatetags/split.py
@@ -0,0 +1,22 @@
+from django.template import Variable, VariableDoesNotExist
+from django import template
+import re
+from pdb import set_trace
+
+register = template.Library()
+
+@register.filter(name='split')
+def split(value, key):
+    """
+        Returns the value turned into a list.
+    """
+    matches = ['länk','klartext']
+
+    ret_dict = {}
+    if all(x in value for x in matches):
+        split_text = re.split(r'\blänk=\b|\bklartext=\b', value)
+        ret_dict['länk'] = split_text[1]
+        ret_dict['klartext'] = split_text[2]
+        return ret_dict
+    else:
+        return value
