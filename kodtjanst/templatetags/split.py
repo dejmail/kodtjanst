34e41de8d2974ccb8a6a8d731609c00a7b4ed66b
Liam Jed Thompson <dejmail@gmail.com>
2022-06-30 13:17:00 +0200

    If using the split tag the string to be split is None, then None is simply returned

--- kodtjanst/templatetags/split.py
+++ kodtjanst/templatetags/split.py
@@ -11,12 +11,13 @@ def split(value, key):
         Returns the value turned into a list.
     """
     matches = ['länk','klartext']
-
-    ret_dict = {}
-    if all(x in value for x in matches):
-        split_text = re.split(r'\blänk=\b|\bklartext=\b', value)
-        ret_dict['länk'] = split_text[1]
-        ret_dict['klartext'] = split_text[2]
-        return ret_dict
-    else:
-        return value
+    if value:
+        ret_dict = {}
+        if all(x in value for x in matches):
+            split_text = re.split(r'\blänk=\b|\bklartext=\b', value)
+            ret_dict['länk'] = split_text[1]
+            ret_dict['klartext'] = split_text[2]
+            return ret_dict
+        else:
+            return value
+    return value    
